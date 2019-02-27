84eca38a8ad6e2b8b6a0ac3082caf8da4a825485
Hitesh Sondhi <hitesh@writingminds.com>
2019-02-27 22:13:22 +0530

    Updates

--- SwiftWebVC/SwiftWebVC.swift
+++ SwiftWebVC/SwiftWebVC.swift
@@ -289,13 +289,13 @@ extension SwiftWebVC: WKUIDelegate {
 extension SwiftWebVC: WKNavigationDelegate {
     
     public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        self.delegate?.didStartLoading(url: webView.url?.absoluteString ?? "")
+        self.delegate?.didStartLoading()
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         updateToolbarItems()
     }
     
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        self.delegate?.didFinishLoading(success: true, url: webView.url?.absoluteString ?? "")
+        self.delegate?.didFinishLoading(success: true)
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
         
         webView.evaluateJavaScript("document.title", completionHandler: {(response, error) in
@@ -307,7 +307,7 @@ extension SwiftWebVC: WKNavigationDelegate {
     }
     
     public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        self.delegate?.didFinishLoading(success: false, url: webView.url?.absoluteString ?? "")
+        self.delegate?.didFinishLoading(success: false)
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
         updateToolbarItems()
     }
