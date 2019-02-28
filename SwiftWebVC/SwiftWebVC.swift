b0658b7e79fbee534e6468a40518dafa97509729
Hitesh Sondhi <hitesh@writingminds.com>
2019-02-28 14:07:48 +0530

    Updates

--- SwiftWebVC/SwiftWebVC.swift
+++ SwiftWebVC/SwiftWebVC.swift
@@ -313,6 +313,6 @@ extension SwiftWebVC: WKNavigationDelegate {
     }
     
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        decisionHandler(.cancel)
+        decisionHandler(.allow)
     }
 }
